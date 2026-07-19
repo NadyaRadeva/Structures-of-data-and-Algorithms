f188c1611d6b9c0c2b0ae29700c3bf680eb83a75
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2026-07-19 20:34:49 +0300

    Create SearchA2DMatrix.cpp

---
+++ New materials/Week 3/Search a 2D Matrix - Leetcode/SearchA2DMatrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int left = 0;
+        int right = matrix.size() - 1;
+
+        int Mi = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(target <= matrix[mid].back()) {
+                right = mid - 1;
+                Mi = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        if(Mi == -1) {
+            return false;
+        }
+
+        left = 0;
+        right = matrix[Mi].size() - 1;
+
+        int Xi = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(target <= matrix[Mi][mid]) {
+                right = mid - 1;
+                Xi = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return matrix[Mi][Xi] == target;
+    }
+};
