74e805071c56b4d140b5cfc283b1f82569e7688a
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2026-07-29 00:16:38 +0300

    Create DailyTemperatures.cpp

---
+++ New materials/Week 5/Daily Temperatures - Leetcode/DailyTemperatures.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        vector<int> results(temperatures.size(), 0);
+
+        for (int i = 0; i < temperatures.size(); ++i) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int index = st.top();
+                st.pop();
+                results[index] = i - index;
+            }
+            st.push(i);
+        }
+
+        return results;
+    }
+};
