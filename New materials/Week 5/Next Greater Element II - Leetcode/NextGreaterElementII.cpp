bb05c2fa932c83970ffa42d6ecda20e802a21fb3
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2026-07-29 00:08:37 +0300

    Create NextGreaterElementII.cpp

---
+++ New materials/Week 5/Next Greater Element II - Leetcode/NextGreaterElementII.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> answer(n, -1);
+        stack<int> st;
+
+        for (int i = 0; i < 2 * n; ++i) {
+
+            while (!st.empty() && nums[st.top()] < nums[i % n]) {
+                answer[st.top()] = nums[i % n];
+                st.pop();
+            }
+
+            if (i < n) {
+                st.push(i);
+            }
+        }
+
+        return answer;
+    }
+};
