0057073748f94eb572814fc60c8b5f99954141e7
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2026-07-31 14:27:50 +0300

    Create SymmetricTree.cpp

---
+++ New materials/Week 6/Symmetric Tree - Leetcode/SymmetricTree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSymmetricHelper(TreeNode* lhs, TreeNode* rhs) {
+        if (!lhs && !rhs) {
+            return true;
+        }
+
+        if (!lhs || !rhs) {
+            return false;
+        }
+
+        if (lhs->val != rhs->val) {
+            return false;
+        }
+
+        return isSymmetricHelper(lhs->left, rhs->right) && isSymmetricHelper(lhs->right, rhs->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+
+        return isSymmetricHelper(root->left, root->right);
+    }
+};
